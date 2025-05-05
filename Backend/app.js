c4f574c58ca1ad7ac6dc8a513ec9db847c2f2f86
jiu702 <jiwoo7764@naver.com>
2025-05-06 00:03:55 +0900

    app.js에 cors 추가

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,20 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const cors = require("cors");
 const authRoutes = require('./src/api/AuthRoute');
+
+
+// cors 허용 
+app.use(cors());
+
+// json 및 form-dataa 처리 
+app.use(express.json()); // json 에 요청 처리 
+app.use(express.urlencoded({extended : true})); // userName , password  파싱 할 때 필요요
 
 //이미지 접근 경로 등록하기
 app.use('/uploads',express.static(path.join(__dirname, './src/uploads')));
-app.use(express.json());
+//라우터 등록하기기
 app.use('/auth', authRoutes);
 
 module.exports = app;
